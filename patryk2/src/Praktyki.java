746326dcb28bea2395afa8a55f22a52af40fb155
PatrykGajecki <daffy1231wp.pl@wp.pl>
2021-02-23 14:41:31 +0100

    projekt

---
+++ patryk2/src/Praktyki.java
@@ -0,0 +1,28 @@
+public class Praktyki {
+
+    public static void main(String[] args){
+
+
+        int A = 5;
+        double B = 5.2;
+        char C = 'g';
+        String D = "ssadsadsa sda ds";
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+
+        int A2 = 3;
+        int B2 = 4;
+        System.out.println(A2*A2+B2*B2);
+
+        int BokA = 7;
+        System.out.println(BokA*BokA*Math.sqrt(3)/4);
+
+
+
+        String imie = "Patryk";
+        String nazwisko = "Gajęcki";
+        System.out.println(imie+" "+nazwisko);
+    }
+}
