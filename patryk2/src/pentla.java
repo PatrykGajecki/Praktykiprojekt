e176e6980639b05dc486c278101cdf33f30cc748
PatrykGajecki <daffy1231wp.pl@wp.pl>
2021-02-24 15:42:16 +0100

    pentla

---
+++ patryk2/src/pentla.java
@@ -0,0 +1,20 @@
+public class pentla
+{
+    public static void main(String[] args) {
+
+
+    char alfabet = 'a';
+
+
+    for (int i = 0; i < 26; i++)
+    {
+      if ( i % 2 == 0)
+          System.out.println(alfabet + "\t");
+        alfabet++;
+    }
+
+
+
+
+    }
+}
---
+++ patryk2/src/pentla2.java
@@ -0,0 +1,21 @@
+public class pentla2
+{
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int f = 9;
+        for(int i = 0; i < f; i++){
+
+            System.out.println(a);
+            int c = b;
+            b = a + b;
+            a = c;
+
+    }
+
+    }
+
+
+
+
+}
