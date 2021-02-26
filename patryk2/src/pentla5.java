6674c0c1d5b182227c60bfa573d625f59009e589
PatrykGajecki <daffy1231wp.pl@wp.pl>
2021-02-26 15:34:01 +0100

    pentla

---
+++ patryk2/src/pentla5.java
@@ -0,0 +1,17 @@
+public class pentla5 {
+        public static void main(String[] args)
+        {
+            int wysokoscTrojkata = 5;
+
+
+            for (int i = 0; i < wysokoscTrojkata; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    System.out.print("X ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
